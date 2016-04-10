3f249d6c3a13f6abcb38a4396f4a57c87b2fcf07
Rahul Krishna <rkbalu524@gmail.com>
2016-04-10 11:30:09 +0530

    Added files for website to be made for Krishna's birthday

---
+++ Krishna_Birthday/script.js
@@ -0,0 +1,48 @@
+
+/**
+ * Created by rahul on 4/8/16.
+ */
+$(document).ready(function () {
+    var window_height=$(window).height();
+    var window_width=$(window).width();
+
+
+    $('.cards-container').css("top",window_height+300);
+
+    $(window).bind("scroll",function () {
+        if ($(window).scrollTop()>100) {
+            $('.page-bg').fadeIn(2000,function () {
+
+            });
+
+        }
+        else {
+            $('.page-bg').fadeOut(2000);
+        }
+
+        if ($(window).scrollTop()>200){
+            $('.page-title').addClass('fixed-title');
+        }
+        else {
+            $('.page-title').removeClass('fixed-title');
+        }
+    });
+
+        //Cyclethru text function.
+        var j = 0;
+        var delay = 2000; //millisecond delay between cycles
+        function cycleThru(){
+            var jmax = $("ul#cyclelist li").length -1;
+            $("ul#cyclelist li:eq(" + j + ")")
+                .animate({"opacity" : "1"} ,400)
+                .animate({"opacity" : "1"}, delay)
+                .animate({"opacity" : "0"}, 400, function(){
+                    (j == jmax) ? j=0 : j++;
+                    cycleThru();
+                });
+        };
+
+        cycleThru();
+        //End of cyclethru
+
+});
