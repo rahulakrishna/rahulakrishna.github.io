4a679af2dbe09043d1e95e4a559f640d3b7461fc
Rahul Krishna <rkbalu524@gmail.com>
2016-04-11 22:16:39 +0530

    Adjusted the slider height according to the viewport

--- Krishna_Birthday/script.js
+++ Krishna_Birthday/script.js
@@ -43,7 +43,7 @@ $(document).ready(function () {
         //End of cyclethruc
 
 
-    $('.slider').slider({full_width: true, indicators:false, height:550});
+    $('.slider').slider({full_width: true, indicators:false, height:window_height-100});
 
 
 });
