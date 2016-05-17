55e28dfa1fe0b5e1ef22b49fb14633cda56240aa
Rahul Krishna <rkbalu524@gmail.com>
2016-05-17 15:16:09 +0530

    React file fix

--- ngReact/app.js
+++ ngReact/app.js
@@ -1,28 +1,47 @@
 var app = angular.module('app', ['react']);
 
-  var PhoneList = React.createClass({
-    propTypes:{
-      phones:React.PropTypes.array.isRequired
-    },
-    render: function () {
-      return (
-        <div className="card-content center">
-          {this.props.phones.map(function (item) {
-            return <div><br/><span> <b>{item.name}</b><br/> {item.snippet}</span></div>
+var Phone=React.createClass({
+  propTypes:{
+    phone:React.PropTypes.object.isRequired
+  },
+  render:function () {
+    var phone=this.props.phone;
+    return(
+        <span>{phone.name}</span>
+    );
+  }
+});
+
+
+var PhoneList = React.createClass({
+  propTypes:{
+    phones:React.PropTypes.array.isRequired
+  },
+  render: function () {
+    var phones=this.props.phones;
+    return (
+        <div className="row">
+          {phones.map(function (item) {
+            return <div className="col m12 s12 push-m0"> <div key={item.age} className="card react-card"><br/><span><div className="card-content"> {item.snippet}</div><div className="card-title">{item.name}</div><br/> </span></div></div>
           })}
         </div>
-      );
-    }
-  });
+    );
+  }
+});
 
-  app.controller('phoneCtrl', ['$scope', '$http', function ($scope, $http) {
-    $http.get('phones.json').success(function (data) {
-      $scope.phones = data;
-    })
-  }]);
+app.controller('phoneCtrl', ['$scope', '$http', function ($scope, $http) {
+  $http.get('phones.json').success(function (data) {
+    $scope.phones = data;
+    $scope.fuck=data;
+  })
+}]);
 
-  app.directive('phonelist',function (reactDirective) {
-    return reactDirective(PhoneList);
-  });
+app.directive('phonelist',function (reactDirective) {
+  return reactDirective(PhoneList);
+});
 
-  angular.bootstrap(document,['app']);
+app.directive('phonename',function (reactDirective) {
+  return reactDirective(Phone);
+});
+
+angular.bootstrap(document,['app']);
