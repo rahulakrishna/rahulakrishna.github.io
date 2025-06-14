08f1c2ef6f6c48d03511b01b14c741e08969d0e8
Rahul Krishna <rkbalu524@gmail.com>
2025-06-14 12:28:34 +0530

    Auto-update: 2025-06-14 12:28:34

--- autopush.sh
+++ autopush.sh
@@ -5,6 +5,8 @@ cd . || exit 1
 
 # Check for any changes (staged, unstaged, or untracked)
 if [[ -n $(git status --porcelain) ]]; then
+  hugo --minify --gc --cleanDestinationDir
+  echo "[$(date)] Changes detected, committing and pushing to remote repository."
   git add .
   git commit -m "Auto-update: $(date '+%Y-%m-%d %H:%M:%S')"
   git push origin master
